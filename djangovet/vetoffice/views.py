4ea0317714ee8d8dd39c0c1e3e59feda4a3d4875
Sean Chang <schang09032003@gmail.com>
2024-06-15 01:07:48 +0100

    feat: Added template + view + url routing for home.html

--- djangovet/vetoffice/views.py
+++ djangovet/vetoffice/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.template import loader
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    context = {"name": "Spot"}
+    return render(request, "vetoffice/home.html", context)
